6db8cfb5344704615c76f0bb5d305d6b4cbd8d64
Chen Roth <rothchen@gmail.com>
2019-05-21 18:45:14 +0300

    render a list of movies, starting with an empty list

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Movie } from './components/Movie/Movie';
-
-interface IMovie {
-    id: number;
-    title: string;
-    description: string;
-}
+import { Movie, IMovieProps } from './components/Movie/Movie';
 
 interface IAppState {
-    movies: IMovie[];
+    movies: IMovieProps[];
 }
 
-class App extends React.Component {
+class App extends React.Component<any, IAppState> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            movies: [{
+                id: 1,
+                title: 'Shrek 3',
+                description: 'Giant green guy gets the princess',
+            }]
+        }
+    }
 
     public render() {
+        const { movies } = this.state;
         return (
             <div className="App">
-                <Movie />
+                {movies.map(movie =>
+                    <Movie key={movie.id} {...movie} />
+                )}
             </div>
         );
     }
